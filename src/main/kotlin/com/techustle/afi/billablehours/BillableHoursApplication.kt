eac21b782e25e2ca8baf525f3c195c9f63d9a4aa
kobbyopoku <godwin.duah@vodafone.com>
2020-08-02 17:58:58 +0000

    adition of swagger documentation plugin

--- src/main/kotlin/com/techustle/afi/billablehours/BillableHoursApplication.kt
+++ src/main/kotlin/com/techustle/afi/billablehours/BillableHoursApplication.kt
@@ -1,19 +1,48 @@
 package com.techustle.afi.billablehours
 
-import com.techustle.afi.billablehours.controller.EmployeeController
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
-//@EnableAutoConfiguration
-@EnableJpaRepositories("com.techustle.afi.billablehours.repository")
-//@ComponentScan(basePackages = "com.techustle.afi.billablehours")
 class BillableHoursApplication
 
-fun main(args: Array<String>) {
-    runApplication<BillableHoursApplication>(*args)
+    fun main(args: Array<String>) {
+        runApplication<BillableHoursApplication>(*args)
+    }
+
+
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiEndPointsInfo())
+    }
+
+    fun apiEndPointsInfo(): ApiInfo {
+        return  ApiInfoBuilder().title("Billable Rating System")
+                .description("A Kotlin REST Api ")
+                .contact( Contact("Godwin Opoku Duah", "https://www.linkedin.com/in/godwin-duah/", "gentlekobby@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 }
